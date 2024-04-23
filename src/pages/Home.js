dd2bc891de801b97e732a34d492cd79193ffb345
brian-ambani <brainambani1@gmail.com>
2024-04-23 06:40:58 +0300

    fetching data from supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,44 @@
 import supabase from "../config/supabaseClient";
+import { useState, useEffect } from "react";
+
+// compponets
+
+import SmoothieCard from "../components/smoothieCard";
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+      if (error) {
+        setFetchError("An error occurred while fetching smoothies");
+        setSmoothies(null);
+        console.error(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order by buttons  */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
